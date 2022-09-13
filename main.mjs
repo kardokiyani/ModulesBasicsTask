23344bddc5bab198f1f1c5411bca243a90de1946
kardokiyani <91535515+kardokiyani@users.noreply.github.com>
2022-09-13 15:15:40 +0200

    Newest Updates

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { addNumbers as newAddNumbersName } from "./module.mjs";
+
+newAddNumbersName();
+
+import calculate from "./module.mjs";
+
+calculate();
---
+++ module.mjs
@@ -0,0 +1,11 @@
+function addNumbers() {}
+
+console.log("This is a module about modules basics");
+
+export { addNumbers };
+
+function calculate() {}
+
+console.log("This task is just training to get to know imports/exports etc");
+
+export default calculate;
